9d7deb24eb76064c80b5d7f805b60291669fb3f7
Koks-creator <73878161+Koks-creator@users.noreply.github.com>
2023-11-07 08:03:25 +0100

    Add files via upload

---
+++ CaesarCipher.py
@@ -0,0 +1,14 @@
+def caesar_cipher(text: str, move: int) -> str:
+    res = ""
+    for char in text:
+        char = ord(char)
+        if 65 <= char <= 90:
+            res += chr((char - ord("A") + move) % 26 + ord("A"))
+        elif 97 <= char <= 133:
+            res += chr((char - ord("a") + move) % 26 + ord("a"))
+        else:
+            res += char
+    return res
+
+
+print(caesar_cipher("AB", -2))
